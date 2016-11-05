89a9f74b6ea34c2aae711a92cae12698b35a04c4
joe <joeida@jpl.nasa.gov>
2016-11-04 18:32:01 -0700

    Initial Commit

---
+++ public/assets/js/app.js
@@ -0,0 +1,86 @@
+// grab the articles as a json
+$.getJSON('/articles', function(data) {
+  // for each one
+  for (var i = 0; i<data.length; i++){
+    // display the apropos information on the page
+    $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
+  }
+});
+
+
+// whenever someone clicks a p tag
+$(document).on('click', 'p', function(){
+  // empty the notes from the note section
+  $('#notes').empty();
+  // save the id from the p tag
+  var thisId = $(this).attr('data-id');
+
+  // now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId,
+  })
+    // with that done, add the note information to the page
+    .done(function( data ) {
+      console.log(data);
+      // the title of the article
+      $('#submitNote').attr('data-id', data._id);
+      $('#notes').attr('data-id', data._id);
+      $('#notes').append('<h3>Title: ' + data.title + '</h3>');
+      // if there's a note in the article
+      if(data.note){
+        // place the title of the note in the title input
+        $('#notes').append('<h4>Note:</h4><p>Title: ' + data.note.title + '</p>');
+        // place the body of the note in the body textarea
+        $('#notes').append('<p>Body: ' + data.note.body + '</p>');
+        $('#notes').append('<button data-id=' + data._id + ' note-id=' + data.note._id + '>Remove</button>');
+
+      } else {
+        console.log('no note');
+      }
+    });
+});
+
+$('#notes').on('click', 'button', function() {
+  var thisId = $(this).attr('data-id');
+  var noteId = $(this).attr('note-id');
+  $.ajax({
+    method: "POST",
+    url: "/articles/delete",
+    data: {
+      thisId: thisId,
+      noteId: noteId
+    }
+  })
+    .done(function(data) {
+      console.log(data);
+      $('#notes').empty();
+    })
+})
+
+// when you click the savenote button
+$(document).on('click', '#submitNote', function(){
+  // grab the id associated with the article from the submit button
+  var thisId = $(this).attr('data-id');
+
+  // run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $('#titleInput').val(), // value taken from title input
+      body: $('#bodyInput').val() // value taken from note textarea
+    }
+  })
+    // with that done
+    .done(function( data ) {
+      // log the response
+      console.log(data);
+      // empty the notes section
+      $('#notes').empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $('#titleInput').val("");
+  $('#bodyInput').val("");
+});
---
+++ server.js
@@ -0,0 +1,40 @@
+// dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+// Notice: Our scraping tools are prepared, too
+var request = require('request');
+var cheerio = require('cheerio');
+//Controllers
+var app_controller = require('./controllers/app_controller');
+
+app.use(methodOverride('_method'));
+// use morgan and bodyparser with our app
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// make public a static dir
+app.use(express.static('public'));
+
+app.use('/', app_controller);
+
+// Database configuration with mongoose
+mongoose.connect('mongodb://localhost/week18day3mongoose');
+var db = mongoose.connection;
+
+// show any mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+// once logged in to the db through mongoose, log a success message
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+module.exports = app;
