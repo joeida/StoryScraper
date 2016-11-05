e6cd7b9c6d1fd93bcc208c1f24baedeb638f3089
joe <joeida@jpl.nasa.gov>
2016-11-04 21:05:11 -0700

    Added heading to articles list

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,7 @@
 // grab the articles as a json
 $.getJSON('/articles', function(data) {
   // for each one
+  $('#articles').append('<h2>Choose Scraped Item</h2>');
   for (var i = 0; i<data.length; i++){
     // display the apropos information on the page
     $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
